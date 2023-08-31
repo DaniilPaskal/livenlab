1ff93fb86ff9e048d0c29446d695e97225b7458c
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-30 23:22:22 -0400

    Added nav toggle button event handler

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const navLinks = document.getElementById('links-container');
+
+function toggleNav() {
+  console.log(navLinks);
+  navLinks.classList.toggle('hide');
+}
+
 function setCopyrightDate() {
     let date = new Date().getFullYear();
     document.querySelector(
