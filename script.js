855aa2618585519a3570a9069eb2d697366b758f
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-19 00:01:37 -0400

    Expanded toggleElement to allow rotating toggle button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function toggleElement(id) {
+function toggleElement(id, buttonId) {
   const element = document.getElementById(id);
   element.classList.toggle('hide');
+
+  if (buttonId) {
+    const button = document.getElementById(buttonId);
+    button.classList.toggle('rotated');
+  }
 }
 
 function setCopyrightDate() {
